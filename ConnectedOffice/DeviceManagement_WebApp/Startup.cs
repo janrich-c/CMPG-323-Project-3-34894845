5daa8dca4ad2315a2ac600267e6a44106aff9eed
Janrich Cronje <janrichc@gmail.com>
2022-09-28 11:44:49 +0200

    added ConnStr

--- ConnectedOffice/DeviceManagement_WebApp/Startup.cs
+++ ConnectedOffice/DeviceManagement_WebApp/Startup.cs
@@ -29,10 +29,10 @@ namespace DeviceManagement_WebApp
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                    Configuration.GetConnectionString("ConnStr")));
             services.AddDbContext<ConnectedOfficeContext>(options =>
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                    Configuration.GetConnectionString("ConnStr")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
